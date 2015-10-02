f3fb7da7f1afaa8a6e58d4de06913c892af5f4fd
pheeellis <chiliphyllie@gmail.com>
2015-10-01 18:01:36 -0700

    updated

--- project3/src/TestHashTable.java
+++ project3/src/TestHashTable.java
@@ -1,20 +1,34 @@
 
 public class TestHashTable {
-    public static void main(String[] args) {
-        HashTable hash = new HashTable(2);
-        hash.incCount("bob");
-        hash.incCount("hello");
-        hash.incCount("bob");
-        hash.incCount("bob");
-        hash.incCount("hello");
-        hash.incCount("java");
-        hash.incCount("data");
 
-        for(DataCount d : hash.getCounts()) {
-            System.out.println(d.count + " " + d.data);
-        }
-
-        System.out.println("Expected: 4");
-        System.out.println("Actual: " + hash.getSize());
-    }
+	public static void main(String[] args)
+	{
+		HashTable hash = new HashTable(3);
+		hash.incCount("bob");
+		hash.incCount("hello");
+		hash.incCount("bob");
+		hash.incCount("bob");
+		hash.incCount("hello");
+		hash.incCount("java");
+		hash.incCount("data");
+		hash.incCount("hello");
+		hash.incCount("java");
+		hash.incCount("data");
+		hash.incCount("dumb");
+		hash.incCount("hate");
+		hash.incCount("what");
+		
+		
+		for (DataCount d: hash.getCounts())
+		{
+			System.out.println(d);
+		}
+		
+		
+		
+		
+		System.out.println("Expected: 7");
+		System.out.println("Actual: " + hash.getSize());
+		
+	}
 }
