d237d182c17b11c24755485055d4707b3a4d1f38
pheeellis <chiliphyllie@gmail.com>
2015-10-05 19:56:45 -0700

    updated

--- project3/src/TestHashTable.java
+++ project3/src/TestHashTable.java
@@ -6,29 +6,51 @@
  */
 public class TestHashTable {
     public static void main(String[] args) {
-        HashTable hash = new HashTable(3);
-        hash.incCount("bob");
-        hash.incCount("hello");
-        hash.incCount("bob");
-        hash.incCount("bob");
-        hash.incCount("hello");
-        hash.incCount("java");
-        hash.incCount("data");
-        hash.incCount("hello");
-        hash.incCount("java");
-        hash.incCount("data");
-        hash.incCount("dumb");
-        hash.incCount("hate");
-        hash.incCount("what");
+        HashTable hash = new HashTable(10);
+		
+		String[] data = {"bob", "hello there", "bob", "bob", "hello there", "javadoc", "data structures","hello there",
+				"javadoc", "dumbo", "computer science", "project3", "computer science", "project3", "phyllislau", "jeremy", "phyllislau", "dumbo"};
+			
+		DataCount d = new DataCount("hello there", 3);
+		DataCount d1 = new DataCount("bob", 3);
+		DataCount d2 = new DataCount("jeremy", 1);
+		DataCount d3 = new DataCount("computer science", 2);
+		DataCount d4 = new DataCount("project3", 2);
+		DataCount d5 = new DataCount("javadoc", 2);
+		DataCount d6 = new DataCount("data structures", 1);
+		DataCount d7 = new DataCount("dumbo", 2);
+		DataCount d8 = new DataCount("phyllislau", 2);
+	
+		
+		DataCount[] expected = {d, d1, d2, d3, d4, d5, d6, d7, d8};
 
-
-        for(DataCount d : hash.getCounts()) {
-            System.out.println(d);
-        }
-
-
-        System.out.println("Expected: 7");
-        System.out.println("Actual: " + hash.getSize());
+		
+		for (String s: data)
+		{
+			hash.incCount(s);
+			
+		}
+	
+		boolean error = false;
+		int k = 0;
+		for(DataCount c: hash.getCounts())
+		{
+			if (!c.equals(expected[k]))
+			{
+				error = false;
+				break;
+			}
+			k++;
+		}
+		if (error)
+		{
+			System.out.println("Test failed");
+			
+		}
+		else
+		{
+			System.out.println("Test passed");
+		}
 
     }
 }
