7a652ae796b42b887c626106af70b1db49e7a5ea
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2015-10-23 23:06:18 -0700

    Initilaized Project 4

---
+++ project4/src/HashTable.java
@@ -0,0 +1,170 @@
+/**
+ * An implementation of hashtable mapping strings to
+ * integers. The hashtable uses separate chaining to
+ * for collision reduction.
+ *
+ * @author Jeremy Asuncion
+ * @author Phyllis Lau
+ */
+public class HashTable implements DataCounter<String> {
+    private static final int DEFAULT_BUCKET_COUNT = 16;
+
+    private Node[] buckets;
+
+    private int size;
+
+    public HashTable() {
+        this(DEFAULT_BUCKET_COUNT);
+    }
+
+    public HashTable(int bucketCount) {
+        this.buckets = new Node[bucketCount];
+        size = 0;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @SuppressWarnings("unchecked")
+    public DataCount<String>[] getCounts() {
+        DataCount<String>[] dataCounts = new DataCount[size];
+        int i = 0;
+        for(Node node : buckets) {
+            for(; node != null; node = node.next) {
+                dataCounts[i++] = new DataCount<>(node.data, node.count);
+            }
+        }
+        return dataCounts;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public int getSize() {
+        return size;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void incCount(String data) {
+        if(calculateLoadFactor() > 0.75) {
+            rehash();
+        }
+        if(insert(buckets, data)) size++;
+    }
+    
+    /**
+     * Helper method to add elements into hashtable
+     * 
+     * @param buckets An array of nodes
+     * @param data The element to add to the hashtable
+     * @return Boolean value depending on whether insertion is successful
+     */
+    private boolean insert(Node[] buckets, String data) {
+        int hash = hashString(data) % buckets.length;
+        Node node = buckets[hash];
+        if(node == null) {
+            buckets[hash] = new Node(data);
+            return true;
+        } else {
+            while(node != null && !node.data.equals(data)) {
+                node = node.next;
+            }
+
+            if(node == null) {
+                buckets[hash] = new Node(data, buckets[hash]);
+                return true;
+            } else {
+                node.count++;
+                return false;
+            }
+        }
+    }
+    
+    /**
+     * Finding the load factor of the hashtable 
+     * 
+     * @return the load factor of the hashtable
+     */
+    private double calculateLoadFactor() {
+        return (double) size / (double) buckets.length;
+    }
+
+    /**
+     * To rehash (create a bigger array of nodes and insert nodes again)
+     */
+    private void rehash() {
+        Node[] buckets = new Node[this.buckets.length * 2];
+        for(Node node : this.buckets) {
+            for(; node != null; node = node.next) {
+                put(buckets, node.data, node.count);
+            }
+        }
+        this.buckets = buckets;
+    }
+    
+    /**
+     * Puts data into its corresponding bucket and updates its count
+     * 
+     * @param buckets The array of nodes
+     * @param data The element to insert
+     * @param count The element's count
+     */
+    private void put(Node[] buckets, String data, int count) {
+        int hash = hashString(data) % buckets.length;
+        Node node = buckets[hash];
+        if(node != null) {
+            while(node != null && !node.data.equals(data)) {
+                node = node.next;
+            }
+            if(node != null) {
+                node.count = count;
+            } else {
+                buckets[hash] = new Node(data, buckets[hash]);
+                buckets[hash].count = count;
+            }
+        } else {
+            buckets[hash] = new Node(data);
+            buckets[hash].count = count;
+        }
+
+    }
+
+    /**
+     * Returns the hash function for inserting elements into hashtable
+     * 
+     * @param str element to insert
+     * @return the index to insert the element 
+     */
+    private int hashString(String str) {
+        final int multiplier = 29;
+        int sum = 0;
+        for(int i = 0; i < str.length(); i++) {
+            sum += str.charAt(i) * multiplier;
+        }
+
+        return sum;
+    }
+    
+    /**
+     * An implementation of a Node
+     */
+    private class Node {
+        private String data;
+        private int    count;
+        private Node   next;
+
+        public Node(String data) {
+            this(data, null);
+        }
+
+        public Node(String data, Node next) {
+            this.data = data;
+            this.count = 1;
+            this.next = next;
+        }
+    }
+
+
+}
---
+++ project4/src/WordCount.java
@@ -0,0 +1,238 @@
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+
+/**
+ * An executable that counts the words in a files and prints out the counts in
+ * descending order.
+ *
+ * @author Jeremy Asuncion
+ * @author Phllis Lau
+ */
+public class WordCount
+{
+    private static final int QUICK_SORT_CUTOFF = 15;
+
+    public static void main(String[] args)
+    {
+        if(args.length != 3)
+        {
+            System.out.println("Usage: [-b | -a | -h] [-is | -qs | -ms] <filename>\n");
+            System.out.println("-b - Use an Unbalanced BST");
+            System.out.println("-a - Use an AVL Tree");
+            System.out.println("-h - Use a Hashtable\n");
+            System.out.println("-is - Sorts using insertion sort");
+            System.out.println("-qs - Sorts using quick sort");
+            System.out.println("-ms - Sorts using merge sort");
+            return;
+        }
+
+        try
+        {
+            switch(args[1])
+            {
+                case "-is":
+                    countWordFrequencies(countWords(args[0], args[2]), WordCount::insertionSort);
+                    break;
+                case "-qs":
+                    countWordFrequencies(countWords(args[0], args[2]), WordCount::quickSort);
+                    break;
+                case "-ms":
+                    countWordFrequencies(countWords(args[0], args[2]), WordCount::mergeSort);
+                    break;
+                default:
+                    System.out.println("Invalid choice for second argument");
+                    break;
+            }
+        }
+        catch(IOException e)
+        {
+            System.out.println("An error occurred when parsing the file!:");
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     * Counts the words in a document.
+     *
+     * @return An array of data couns
+     * @throws IOException Thrown if there's an exception wile reading
+     */
+    public static DataCount<String>[] countWords(String dataStructure, String filename) throws IOException
+    {
+        FileWordReader fileWordReader = new FileWordReader(filename);
+        DataCounter<String> wordCounter;
+
+        switch(dataStructure)
+        {
+            case "-b":
+                wordCounter = new BinarySearchTree<>();
+                break;
+            case "-a":
+                wordCounter = new AVLTree<>();
+                break;
+            case "-h":
+                wordCounter = new HashTable();
+                break;
+            default:
+                wordCounter = new BinarySearchTree<>();
+                System.out.println("Invalid data structure. Using BST by default.");
+                break;
+        }
+
+        String word;
+        while((word = fileWordReader.nextWord()) != null)
+        {
+            wordCounter.incCount(word);
+        }
+
+        return wordCounter.getCounts();
+    }
+
+    /**
+     * Prints the word counts given an array of data counts.
+     *
+     * @param dataCounts Data counts
+     * @param <E>        Some type
+     */
+    private static <E> void printWordCounts(DataCount<E>[] dataCounts)
+    {
+        Arrays.stream(dataCounts)
+              .forEach(count -> {
+                  System.out.format("%d %s\n", count.count, count.data);
+              });
+    }
+
+    /**
+     * Counts the word frequencies in a document and lists them
+     * first by frequency, then lexicographically.
+     */
+    private static void countWordFrequencies(DataCount<String>[] dataCounts,
+                                             BiConsumer<DataCount<String>[], Comparator<DataCount<String>>> sortingFunc)
+    {
+        sortingFunc.accept(dataCounts, (count1, count2) -> count2.count - count1.count);
+
+        System.out.println("Ordered by Frequency:");
+        printWordCounts(dataCounts);
+
+        sortingFunc.accept(dataCounts, (count1, count2) -> count1.data.compareTo(count2.data));
+
+        System.out.println("\nOrdered Lexicographically:");
+        printWordCounts(dataCounts);
+    }
+
+    /**
+     * Prints the number of unique words in a document.
+     */
+    private static void countUniqueWords(DataCount<String>[] dataCounts)
+    {
+        System.out.println("Unique words: " + dataCounts.length);
+    }
+
+    /**
+     * Convenience method for sorting an array of data counts using quick sort and a
+     * comparator.
+     *
+     * @param dataCounts The array of data counts
+     * @param comparator The comparator to compare data counts
+     * @param <E>        Some type
+     * @see #insertionSort(DataCount[], int, int, Comparator)
+     */
+    private static <E> void insertionSort(DataCount<E>[] dataCounts, Comparator<DataCount<E>> comparator)
+    {
+        insertionSort(dataCounts, 0, dataCounts.length - 1, comparator);
+    }
+
+    /**
+     * Implementation of insertion sort. Sorts an array of data counts using a comparator
+     * over the range {@code [low, high]} within the array, where 0 <= low <= high < n.
+     *
+     * @param dataCounts The array of data counts
+     * @param low        The lower bound
+     * @param high       The upper bound
+     * @param comparator The comparator to compare data counts
+     * @param <E>        Some type
+     */
+    private static <E> void insertionSort(DataCount<E>[] dataCounts,
+                                          int low, int high,
+                                          Comparator<DataCount<E>> comparator)
+    {
+        for(int i = low + 1; i <= high; i++)
+        {
+            DataCount<E> key = dataCounts[i];
+            int j = i - 1;
+            while(j >= 0 && comparator.compare(key, dataCounts[j]) < 0)
+            {
+                dataCounts[j + 1] = dataCounts[j--];
+            }
+            dataCounts[j + 1] = key;
+        }
+    }
+
+    private static <E> void quickSort(DataCount<E>[] dataCounts, Comparator<DataCount<E>> comparator) {
+        // TODO Implemented by Phil
+    }
+
+    /**
+     * Implementation of merge sort algorithm. Sorts an array of data counts
+     * using a comparator.
+     *
+     * @param dataCounts The array of data counts
+     * @param comparator The comparator to compare data counts
+     * @param <E>        Some type
+     */
+    private static <E> void mergeSort(DataCount<E>[] dataCounts, Comparator<DataCount<E>> comparator)
+    {
+        if(dataCounts.length > 1)
+        {
+            int mid = dataCounts.length / 2;
+            DataCount<E>[] left = Arrays.copyOfRange(dataCounts, 0, mid);
+            DataCount<E>[] right = Arrays.copyOfRange(dataCounts, mid, dataCounts.length);
+
+            mergeSort(left, comparator);
+            mergeSort(right, comparator);
+            merge(dataCounts, left, right, comparator);
+        }
+    }
+
+    /**
+     * Merges the left and right data counts back into the original array.
+     * It's assumed that {@code left + right = dataCounts}.
+     *
+     * @param dataCounts The original array
+     * @param left       The left side of the original array
+     * @param right      The right side of the original array
+     * @param comparator The comparator to compare data counts
+     * @param <E>        Some type
+     */
+    private static <E> void merge(DataCount<E>[] dataCounts,
+                                  DataCount<E>[] left, DataCount<E>[] right,
+                                  Comparator<DataCount<E>> comparator)
+    {
+        int i = 0, j = 0;
+        for(int k = 0; k < dataCounts.length; k++)
+        {
+            if(i < left.length && j < right.length)
+            {
+                if(comparator.compare(left[i], right[j]) <= 0)
+                {
+                    dataCounts[k] = left[i++];
+                }
+                else
+                {
+                    dataCounts[k] = right[j++];
+                }
+            }
+            else if(i < left.length)
+            {
+                dataCounts[k] = left[i++];
+            }
+            else if(j < right.length)
+            {
+                dataCounts[k] = right[j++];
+            }
+        }
+    }
+}
